0d1cbf4bf2226274e3639054080a14d22f349b3e
Colin Hong <colin.hong18@gmail.com>
2024-03-31 22:16:11 +0800

    Added script to encode a set of URLs to pass as query string

---
+++ test_url_encoding.py
@@ -0,0 +1,26 @@
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+
+    # Check if at least one URL is provided
+    if len(sys.argv) < 2:
+        print("Usage: python test_url_encoding.py <url1> <url2> ...")
+        sys.exit(1)
+
+    url_array = []
+    # Iterate over each URL argument
+    for url in sys.argv[1:]:
+        url_array.append(url)
+
+    url_array_string = str(url_array)
+    encoded_url_array_string = urllib.parse.quote(url_array_string, safe='')
+    print(encoded_url_array_string)
+
+    # # Decode the parsed string
+    # decoded_url_array_string = urllib.parse.unquote(encoded_url_array_string)
+    # print(decoded_url_array_string)
+
+    # # Convert the string back to a list
+    # decoded_url_array = eval(decoded_url_array_string)
+    # print(type(decoded_url_array))
